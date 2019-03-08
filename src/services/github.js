8a45df63c1d5aaefd8ac83f9d14d1d89902d0d19
John Kueh <yoongfook@gmail.com>
2019-03-08 15:03:56 +1100

    Able to search for tasks on Asana based on github commit

--- src/services/github.js
+++ src/services/github.js
@@ -1,6 +1,8 @@
 import 'dotenv/config';
+import _ from 'lodash';
 import Octokit from '@octokit/rest';
 import crypto from 'crypto';
+import { searchTask } from '../services/asana';
 
 const octokit = new Octokit({
   auth: `token ${process.env.GITHUB_PATOKEN}`
@@ -8,16 +10,16 @@ const octokit = new Octokit({
 
 export const listHooks = async () => {
   const data = await octokit.repos.listHooks({
-    owner: 'yoongfook',
-    repo: 'featureready'
+    owner: process.env.GITHUB_OWNER_NAME,
+    repo: process.env.GITHUB_REPO_NAME
   });
   console.log(data);
 };
 
 export const createHook = async () => {
   const result = await octokit.repos.createHook({
-    owner: 'yoongfook',
-    repo: 'featureready',
+    owner: process.env.GITHUB_OWNER_NAME,
+    repo: process.env.GITHUB_REPO_NAME,
     config: {
       content_type: 'application/json',
       url: `${process.env.BASE_URL}/webhooks/github`,
@@ -29,11 +31,40 @@ export const createHook = async () => {
   console.log(result);
 };
 
+const findTaskId = commitMessage => {
+  const match = commitMessage.match(/\[(.+?)\]/);
+  if (match) {
+    return match[1];
+  }
+
+  return null;
+};
+
 export const handleHooks = req => {
   const body = JSON.parse(req.body);
   const type = req.headers['x-github-event'];
   console.log('Github webhook - ', type);
-  // console.log('github webhook', body);
+  switch (type) {
+    case 'push':
+      console.log('handle push');
+      _.each(body.commits, async commit => {
+        const taskId = findTaskId(commit.message);
+        if (taskId) {
+          const asanaTask = await searchTask(taskId);
+          console.log(asanaTask);
+        }
+        console.log('taskId', findTaskId(commit.message));
+      });
+      break;
+    case 'pull_request':
+      console.log('handle PR');
+      break;
+    case 'deployment_status':
+      console.log('handle deploy');
+      break;
+    default:
+      console.log('unrecognized event');
+  }
 };
 
 export const verifyGitHub = req => {
