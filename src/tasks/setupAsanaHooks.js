33a10876ebaebb26bcf291d23f46545fa8e5268a
michPl <plehanov_michael@mail.ru>
2020-05-26 12:36:59 +0200

    Added catches to async functions calls

--- src/tasks/setupAsanaHooks.js
+++ src/tasks/setupAsanaHooks.js
@@ -2,14 +2,22 @@ import _ from 'lodash';
 import { createHooks, getHooks } from '../services/asana';
 
 console.log('Creating Asana hooks');
-createHooks().then(async () => {
-  const res = await getHooks();
-  _.each(res.data, ({ gid, active, target }) => {
-    console.log('--- Hooks ---');
-    console.log('gid    : ', gid);
-    console.log('active : ', active);
-    console.log('target : ', target);
-    console.log('---');
+createHooks()
+  .then(async () => {
+    const res = await getHooks();
+    _.each(res.data, ({ gid, active, target }) => {
+      console.log('--- Hooks ---');
+      console.log('gid    : ', gid);
+      console.log('active : ', active);
+      console.log('target : ', target);
+      console.log('---');
+    });
+    console.log('Asana hooks successfully created');
+  })
+  .catch(({ message, stack, ...rest }) => {
+    console.error('--- Creating Asana hooks errors ---');
+    console.error('message         : ', message);
+    console.error('stack           : ', stack);
+    console.error('additional info : ', JSON.stringify(rest));
+    console.error('---');
   });
-  console.log('Asana hooks successfully created');
-});
