490bf84c2a0f4b01ad2ab5084a83bf8b1640f250
Tanner <tanner.hallman@gmail.com>
2020-06-27 12:48:12 -0400

    added catch block from https://github.com/johnkueh/asana-github-tools/pull/15

--- src/tasks/getHooks.js
+++ src/tasks/getHooks.js
@@ -1,12 +1,21 @@
 import _ from 'lodash';
 import { getHooks } from '../services/asana';
 
-getHooks().then(res => {
-  _.each(res.data, ({ gid, active, target }) => {
-    console.log('--- Hooks ---');
-    console.log('gid    : ', gid);
-    console.log('active : ', active);
-    console.log('target : ', target);
-    console.log('---');
+getHooks()
+  .then(res => {
+    _.each(res.data, ({ gid, active, target }) => {
+      console.log('--- Hooks ---');
+      console.log('gid    : ', gid);
+      console.log('active : ', active);
+      console.log('target : ', target);
+      console.log('---');
+    });
+  })
+  .catch(({ message, stack, ...rest }) => {
+    console.error('--- Getting hooks errors ---');
+    console.error('message         : ', message);
+    console.error('stack           : ', stack);
+    console.error('additional info : ', JSON.stringify(rest));
+    console.error('---');
   });
 });
